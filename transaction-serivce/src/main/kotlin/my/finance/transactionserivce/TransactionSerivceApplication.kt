7fe838569be8b3f821a12148cbdf4a3901a68228
Asulan Maksut <a.maksut@strongte.am>
2023-10-10 13:52:59 +0600

    init transaction-service

---
+++ transaction-serivce/src/main/kotlin/my/finance/transactionserivce/TransactionSerivceApplication.kt
@@ -0,0 +1,11 @@
+package my.finance.transactionserivce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TransactionSerivceApplication
+
+fun main(args: Array<String>) {
+	runApplication<TransactionSerivceApplication>(*args)
+}
---
+++ transaction-serivce/src/test/kotlin/my/finance/transactionserivce/TransactionSerivceApplicationTests.kt
@@ -0,0 +1,13 @@
+package my.finance.transactionserivce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TransactionSerivceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

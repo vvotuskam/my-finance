d1cca4e60928c41d24cbcca7304a27f2a2bac966
Asulan Maksut <a.maksut@strongte.am>
2023-10-10 16:12:00 +0600

    add transaction support for transfer

--- account-service/src/main/kotlin/my/finance/accountservice/feature/transaction/data/TransactionService.kt
+++ account-service/src/main/kotlin/my/finance/accountservice/feature/transaction/data/TransactionService.kt
@@ -1,9 +1,15 @@
 package my.finance.accountservice.feature.transaction.data
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class TransactionService(
     private val transactionRepository: TransactionRepository
 ) {
+
+    @Transactional
+    fun save(transaction: Transaction): Transaction {
+        return transactionRepository.save(transaction)
+    }
 }
--- account-service/src/main/kotlin/my/finance/accountservice/feature/transaction/domain/usecase/TransferUseCase.kt
+++ account-service/src/main/kotlin/my/finance/accountservice/feature/transaction/domain/usecase/TransferUseCase.kt
@@ -4,20 +4,22 @@ import my.finance.accountservice.core.data.entity.User
 import my.finance.accountservice.core.domain.exception.BusinessException
 import my.finance.accountservice.core.domain.usecase.UseCase
 import my.finance.accountservice.core.rest.dto.SuccessResponse
-import my.finance.accountservice.feature.account.data.Account
 import my.finance.accountservice.feature.account.data.AccountService
+import my.finance.accountservice.feature.transaction.data.Transaction
+import my.finance.accountservice.feature.transaction.data.TransactionService
 import my.finance.accountservice.feature.transaction.domain.failure.AccountFromNotFoundFailure
 import my.finance.accountservice.feature.transaction.domain.failure.AccountToNotFoundFailure
 import my.finance.accountservice.feature.transaction.domain.failure.InsufficientFundsFailure
+import my.finance.accountservice.feature.transaction.domain.failure.SameAccountsFailure
 import my.finance.accountservice.feature.transaction.domain.usecase.TransferUseCase.TransferParams
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
-import java.nio.DoubleBuffer
 import java.util.UUID
 
 @Component
 class TransferUseCase(
-    private val accountService: AccountService
+    private val accountService: AccountService,
+    private val transactionService: TransactionService,
 ) : UseCase<TransferParams, SuccessResponse> {
 
     data class TransferParams(
@@ -30,6 +32,8 @@ class TransferUseCase(
     @Transactional
     override fun invoke(params: TransferParams): SuccessResponse {
         val (user, fromId, toId, amount) = params
+
+        if (fromId == toId) throw BusinessException(SameAccountsFailure())
 
         val from = accountService.findByIdAndUser(fromId, user)
             ?: throw BusinessException(AccountFromNotFoundFailure())
@@ -49,6 +53,14 @@ class TransferUseCase(
 
         accountService.save(updatedFrom)
         accountService.save(updatedTo)
+
+        val transaction = Transaction(
+            from = updatedFrom,
+            to = updatedTo,
+            amount = amount
+        )
+
+        transactionService.save(transaction)
 
         return SuccessResponse(status = "Transferred")
     }

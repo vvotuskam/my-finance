f67f110eef0097c59ae0710c757a6eea2902a95d
Asulan Maksut <a.maksut@strongte.am>
2023-10-27 15:47:14 +0600

    account service db migration refactor

--- account-service/src/main/resources/db/changelog/changeset/create_account_table.sql
+++ account-service/src/main/resources/db/changelog/changeset/create_account_table.sql
@@ -1,4 +1,4 @@
-create table if not exists otp_codes(
+create table if not exists accounts(
     id          uuid            primary key not null default gen_random_uuid(),
     name        varchar(255)    not null,
     amount      float           not null,
